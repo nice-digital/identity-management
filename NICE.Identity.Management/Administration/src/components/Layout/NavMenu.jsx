e6e1a2af792f4f8ae94b4b7562dad5f0d1fb68a3
AlzAmido <alessio.fimognari@amido.com>
2019-02-25 15:03:17 +0000

    adding logout link

--- NICE.Identity.Management/Administration/src/components/Layout/NavMenu.jsx
+++ NICE.Identity.Management/Administration/src/components/Layout/NavMenu.jsx
@@ -1,16 +1,21 @@
-﻿import React from 'react';
-import {LefthandMenuContainer, StyledLink, LefthandMenuWrapper} from "./component";
+﻿import React from "react";
+import {
+  LefthandMenuContainer,
+  StyledLink,
+  LefthandMenuWrapper
+} from "./component";
 
 export class NavMenu extends React.Component {
   render() {
     return (
       <LefthandMenuWrapper>
         <h1>User Admin Portal</h1>
+        <a href="https://alpha-identity.nice.org.uk/logout"> Logout </a>
         <LefthandMenuContainer>
-            <StyledLink to="/">Users</StyledLink>
-            <StyledLink to="/roles">Roles</StyledLink>
-            <StyledLink to="/realms">Realms</StyledLink>
-        </LefthandMenuContainer> 
+          <StyledLink to="/">Users</StyledLink>
+          <StyledLink to="/roles">Roles</StyledLink>
+          <StyledLink to="/realms">Realms</StyledLink>
+        </LefthandMenuContainer>
       </LefthandMenuWrapper>
     );
   }
