13938e668666376c9fdc7c80eff8f18dc0cfe429
Cheguegallagher <martin.gallagher@nice.org.uk>
2019-03-11 10:44:38 +0000

    Adding in teamcity setup to project

--- functional-tests/wdio.teamcity.conf.js
+++ functional-tests/wdio.teamcity.conf.js
@@ -7,15 +7,20 @@ config.services = [];
 // Run headless on TeamCity
 config.capabilities = [
     {
+        acceptInsecureCerts: true, // Because of self-signed cert inside Docker
+        acceptSslCerts: true,
+        maxInstances: 2,
         browserName: "chrome",
         chromeOptions: {
             args: ["--headless", "--window-size=1366,768"]
         }
-    }
+    }//,
     // {
+    // 	acceptInsecureCerts: true, // Because of self-signed cert inside Docker
+    // 	acceptSslCerts: true,
     //     browserName: "firefox",
     //     "moz:firefoxOptions": {
-    //         args: ["-headless"]
+    // 		args: ["-headless"]
     //     }
     // }
 ];
