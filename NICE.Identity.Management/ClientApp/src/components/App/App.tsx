6b725ff1228b6c10243e37c780962568134fc588
Warren Keith <warren.keith@nice.org.uk>
2019-08-07 16:56:19 +0100

    IDAM-253 Demo tests for app

--- NICE.Identity.Management/ClientApp/src/components/App/App.tsx
+++ NICE.Identity.Management/ClientApp/src/components/App/App.tsx
@@ -3,23 +3,45 @@ import logo from "./logo.svg";
 import styles from "./App.module.scss";
 import { Tag } from "@nice-digital/nds-tag";
 
-const App: React.FC = () => {
-	return (
-		<div className={styles.App}>
-			<header>
-				<img src={logo} alt="logo" />
-				<p>
-					This is a <Tag alpha>tag</Tag> right here.
-					<Tag outline flush>
-						Things!
-					</Tag>
-					<span>
-						<Tag flush>Hello</Tag>
-					</span>
-				</p>
-			</header>
-		</div>
-	);
-};
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	lookupNameById(id, event) {
+		event.preventDefault();
+		if (id === 121) return "Malcolm";
+		if (id === 232) return "jess";
+		return "user not found";
+	}
+
+	render() {
+		if (!this.props.loggedIn) return <h1>you need to log in!</h1>;
+		if (this.props.loggedIn) {
+			return (
+				<div className={styles.App}>
+					<button onClick={event => this.lookupNameById(121, event)} />
+					<header>
+						<img src={logo} alt="logo" />
+						<h1>Hello</h1>
+						<h2>There</h2>
+						{this.props.monkey === "gerald" && (
+							<p>this monkey is called gerald</p>
+						)}
+						<p>
+							This is a <Tag alpha>tag</Tag> right here.
+							<Tag outline flush>
+								Things!
+							</Tag>
+							<span>
+								<Tag flush>Hello!</Tag>
+							</span>
+						</p>
+					</header>
+				</div>
+			);
+		}
+	}
+}
 
 export default App;
--- NICE.Identity.Management/ClientApp/src/components/App/__tests__/App.test.tsx
+++ NICE.Identity.Management/ClientApp/src/components/App/__tests__/App.test.tsx
@@ -1,9 +1,39 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { shallow, mount } from "enzyme";
+import toJson from "enzyme-to-json";
 import App from "../App";
 
-it("renders without crashing", () => {
-	const div = document.createElement("div");
-	ReactDOM.render(<App />, div);
-	ReactDOM.unmountComponentAtNode(div);
+const fakeEvent = {
+	preventDefault: function() {},
+};
+
+it("shows login sceren if logged out", () => {
+	const wrapper = shallow(<App loggedIn={false} />);
+	expect(toJson(wrapper)).toMatchSnapshot();
+});
+
+it("shows secret data if logged in", () => {
+	const wrapper = shallow(<App loggedIn={true} />);
+	expect(toJson(wrapper)).toMatchSnapshot();
+});
+
+it("lookup method should return correct names", () => {
+	const lookupMethod = new App();
+	const result = lookupMethod.lookupNameById(121, fakeEvent);
+	expect(result).toEqual("Malcolm");
+	const another = lookupMethod.lookupNameById(434343434, fakeEvent);
+	expect(another).toEqual("user not found");
+});
+
+it("tests the click handler", () => {
+	//assemble
+	const wrapper = mount(<App loggedIn={true} />);
+	const spy = jest.spyOn(wrapper.instance(), "lookupNameById");
+	expect(spy).toHaveBeenCalledTimes(0);
+
+	//act
+	wrapper.find("button").simulate("click");
+
+	//assert
+	expect(spy).toHaveBeenCalled();
 });
