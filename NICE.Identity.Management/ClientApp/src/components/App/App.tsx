85c3d3e166ed98ed314868593ac10c9ac1a9c505
John Holland <john.holland@nice.org.uk>
2019-08-08 13:01:25 +0100

    IDAM-253 - Tidy up for PR
    ... and chucked in dummy list component

--- NICE.Identity.Management/ClientApp/src/components/App/App.tsx
+++ NICE.Identity.Management/ClientApp/src/components/App/App.tsx
@@ -2,45 +2,30 @@ import React from "react";
 import logo from "./logo.svg";
 import styles from "./App.module.scss";
 import { Tag } from "@nice-digital/nds-tag";
+import List from "./../List/List";
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
-	lookupNameById(id, event) {
-		event.preventDefault();
-		if (id === 121) return "Malcolm";
-		if (id === 232) return "jess";
-		return "user not found";
-	}
-
 	render() {
-		if (!this.props.loggedIn) return <h1>you need to log in!</h1>;
-		if (this.props.loggedIn) {
-			return (
-				<div className={styles.App}>
-					<button onClick={event => this.lookupNameById(121, event)} />
-					<header>
-						<img src={logo} alt="logo" />
-						<h1>Hello</h1>
-						<h2>There</h2>
-						{this.props.monkey === "gerald" && (
-							<p>this monkey is called gerald</p>
-						)}
-						<p>
-							This is a <Tag alpha>tag</Tag> right here.
-							<Tag outline flush>
-								Things!
-							</Tag>
-							<span>
-								<Tag flush>Hello!</Tag>
-							</span>
-						</p>
-					</header>
-				</div>
-			);
-		}
+		return (
+			<div className={styles.App}>
+				<header>
+					<img src={logo} alt="logo" />
+					<h1>Hello</h1>
+					<h2>There</h2>
+
+					<p>
+						This is a <Tag alpha>tag</Tag> right here.
+						<Tag outline flush>
+							Things!
+						</Tag>
+						<span>
+							<Tag flush>Hello!</Tag>
+						</span>
+					</p>
+				</header>
+				<List />
+			</div>
+		);
 	}
 }
 
---
+++ NICE.Identity.Management/ClientApp/src/components/List/List.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class List extends React.Component {
+	render() {
+		return (
+			<div>
+				<p>Something</p>
+			</div>
+		);
+	}
+}
+
+export default List;
