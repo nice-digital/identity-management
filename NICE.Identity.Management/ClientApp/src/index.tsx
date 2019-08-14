dbb47479c2ac1409f92c988b6a90ae3beb5780bb
John Holland <john.holland@nice.org.uk>
2019-08-14 11:20:08 +0100

    IDAM-253 - Commented out design system styles

--- NICE.Identity.Management/ClientApp/src/index.tsx
+++ NICE.Identity.Management/ClientApp/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "@nice-digital/design-system/scss/nice-design-system.scss";
+//import "@nice-digital/design-system/scss/nice-design-system.scss";
 import App from "./components/App/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
