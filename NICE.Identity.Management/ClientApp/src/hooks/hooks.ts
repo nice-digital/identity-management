4443eac49ebebbf245a970c1ccfb547974da9a3c
John Holland <john.holland@nice.org.uk>
2019-08-20 11:16:20 +0100

    IDAM-258 - Fixed fecth hook

--- NICE.Identity.Management/ClientApp/src/hooks/hooks.ts
+++ NICE.Identity.Management/ClientApp/src/hooks/hooks.ts
@@ -1,26 +1,28 @@
 import { useState, useEffect } from "react";
 
 type User = {
-	_id: string;
-	name: string;
+	id: number;
 	email: string;
+	user_id: string;
+	given_name: string;
+	family_name: string;
 };
 
 export function useFetch(url: string): [Array<User>, boolean] {
 	const [data, setData] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
 
-	async function fetchUrl() {
-		const response = await fetch(url);
-		const json = await response.json();
-		setData(json);
-		setIsLoading(false);
-	}
-
 	// https://reactjs.org/docs/hooks-effect.html
 	useEffect(() => {
+		async function fetchUrl() {
+			const response = await fetch(url);
+			const json = await response.json();
+			setData(json);
+			setIsLoading(false);
+		}
+
 		fetchUrl(); // If you want to run an effect and clean it up only once (on mount and unmount), you can pass an empty array ([]) as a second argument
-	});
+	}, [url]);
 
 	return [data, isLoading];
 }
