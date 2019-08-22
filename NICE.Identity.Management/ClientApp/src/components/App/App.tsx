2e06f320eb65a2eacab9f34c986effd53d52418a
John Holland <john.holland@nice.org.uk>
2019-08-22 15:33:20 +0100

    IDAM-268 - Tidy up

--- NICE.Identity.Management/ClientApp/src/components/App/App.tsx
+++ NICE.Identity.Management/ClientApp/src/components/App/App.tsx
@@ -1,28 +1,46 @@
 import React from "react";
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Header, Footer } from "@nice-digital/global-nav";
+import { Breadcrumbs, Breadcrumb } from "@nice-digital/nds-breadcrumbs";
+
 import { UsersList } from "../../views/UsersList/UsersList";
 import { User } from "./../../views/User/User";
 import { UserRoles } from "./../../views/UserRoles/UserRoles";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Header, Footer } from "@nice-digital/global-nav";
 
-class App extends React.Component {
+export class App extends React.Component {
 	render() {
 		return (
 			<Router>
 				<Header search={false} />
 
-				<nav>
-					<ul>
-						<li>
-							<Link to="/">Home</Link>
-						</li>
-						<li>
-							<Link to="/users">Users</Link>
-						</li>
-					</ul>
-				</nav>
-
 				<div className="container">
+					<Route
+						path="/"
+						exact
+						render={() => (
+							<>
+								<Breadcrumbs>
+									<Breadcrumb>Home</Breadcrumb>
+								</Breadcrumbs>
+								<div className="page-header">
+									<h1 className="page-header__heading">Identity admin</h1>
+									<p className="page-header__lead">
+										Admin site for managing users, services and roles for Auth0
+										identity
+									</p>
+								</div>
+
+								<nav>
+									<ul className="list list--piped">
+										<li>
+											<Link to="/users">Users</Link>
+										</li>
+									</ul>
+								</nav>
+							</>
+						)}
+					/>
 					<Route path="/users" exact component={UsersList} />
 					<Route path="/users/:id" exact component={User} />
 					<Route path="/users/:id/roles" exact component={User} />
@@ -38,5 +56,3 @@ class App extends React.Component {
 		);
 	}
 }
-
-export default App;
--- NICE.Identity.Management/ClientApp/src/components/Filter/Filter.tsx
+++ NICE.Identity.Management/ClientApp/src/components/Filter/Filter.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Panel } from "@nice-digital/nds-panel";
-import { Radio, Input } from "@nice-digital/nds-forms";
+import { Input } from "@nice-digital/nds-forms";
 
 export const Filter = () => (
 	<Panel>
 		<p>Filter options</p>
 		<Input type="text" label="Search" unique="userSearch" name="user-search" />
-		<Radio label="Label here" />
 	</Panel>
 );
