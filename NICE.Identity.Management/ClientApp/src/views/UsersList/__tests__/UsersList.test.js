6266c131435129e068b4e696be697960d202ba06
John Holland <john.holland@nice.org.uk>
2019-08-29 12:06:42 +0100

    Amends for UserList tests

--- NICE.Identity.Management/ClientApp/src/views/UsersList/__tests__/UsersList.test.js
+++ NICE.Identity.Management/ClientApp/src/views/UsersList/__tests__/UsersList.test.js
@@ -9,13 +9,13 @@ import { nextTick } from "../../../utils/nextTick";
 
 afterEach(fetchMock.reset);
 
-it("should mount without crashing", () => {
+it("should show loading message before data has been loaded", () => {
 	fetchMock.get("*", {});
 	const wrapper = shallow(<UsersList />);
-	expect(wrapper).toHaveLength(1);
+	expect(wrapper.find("p").text()).toEqual("Loading...");
 });
 
-it("should match the snapshot when loading", async () => {
+it("should match the snapshot after data has been loaded", async () => {
 	fetchMock.get("*", users);
 	const wrapper = mount(
 		<MemoryRouter>
