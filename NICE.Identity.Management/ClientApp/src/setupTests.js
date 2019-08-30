7f0450227ebda645b92cbf3ba557b91b517bc50d
John Holland <john.holland@nice.org.uk>
2019-08-30 17:01:50 +0100

    IDAM-258 - Added citation for global fetch mock

--- NICE.Identity.Management/ClientApp/src/setupTests.js
+++ NICE.Identity.Management/ClientApp/src/setupTests.js
@@ -3,13 +3,13 @@ import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
 
+// https://twitter.com/kentcdodds/status/1163644392789250048?s=19
 beforeEach(() => {
 	jest.spyOn(window, "fetch").mockImplementation((...args) => {
 		console.warn("window.fetch is not mocked for this call", ...args);
 		throw new Error("This must be mocked!");
 	});
 });
-
 afterEach(() => {
 	window.fetch.mockRestore();
 });
