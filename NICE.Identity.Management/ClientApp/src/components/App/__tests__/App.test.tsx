32c0030d1643e67120cd4fd5d9304637803e2cb6
John Holland <john.holland@nice.org.uk>
2019-08-30 17:04:56 +0100

    IDAM-258 - Added catch-all mount test

--- NICE.Identity.Management/ClientApp/src/components/App/__tests__/App.test.tsx
+++ NICE.Identity.Management/ClientApp/src/components/App/__tests__/App.test.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
 import toJson from "enzyme-to-json";
-import App from "../App";
-it("should mount without crashing", () => {});
+import { App } from "../App";
+
+it("should mount without crashing", () => {
+	const wrapper = mount(<App />);
+	expect(wrapper).toHaveLength(1);
+	wrapper.unmount();
+});
