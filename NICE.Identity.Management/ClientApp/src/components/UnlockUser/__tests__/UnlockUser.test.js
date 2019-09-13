1d47ef6d3daa366bbc307374d40c81eaaf805abe
John Holland <john.holland@nice.org.uk>
2019-09-13 13:14:55 +0100

    IDAM-9 - Tests for User and UnlockUser components

--- NICE.Identity.Management/ClientApp/src/components/UnlockUser/__tests__/UnlockUser.test.js
+++ NICE.Identity.Management/ClientApp/src/components/UnlockUser/__tests__/UnlockUser.test.js
@@ -1,11 +1,33 @@
 import React from "react";
 import { mount, shallow } from "enzyme";
 import fetchMock from "fetch-mock";
-import { toJson } from "enzyme-to-json";
-import { UnlockUser } from "../UnlockUser";
 
-it("should mount without crashing", () => {
-	const wrapper = mount(<UnlockUser />);
-	expect(wrapper).toHaveLength(1);
-	wrapper.unmount();
+import { UnlockUser } from "../UnlockUser";
+import { Endpoints } from "../../../data/endpoints";
+//import singleUser from "./singleUser.json";
+
+const userProps = {
+	id: 1,
+	blocked: true,
+	onToggleLock: jest.fn(),
+};
+
+afterEach(fetchMock.reset);
+
+it("should trigger the fetchPatchData with correct arguments when when the lock/unlock button is clicked", () => {
+	fetchMock.patch("*", {});
+	const wrapper = mount(<UnlockUser {...userProps} />);
+	const apiUrl = Endpoints.editUser(userProps.id);
+	const spy = jest.spyOn(wrapper.instance(), "fetchPatchData");
+	expect(spy).toHaveBeenCalledTimes(0);
+	wrapper.find("button").simulate("click");
+	expect(spy).toHaveBeenCalledWith(apiUrl, userProps.blocked);
+});
+
+it("should trigger onToggleLock prop function once fetch is complete", async () => {
+	fetchMock.patch("*", {});
+	const apiUrl = Endpoints.editUser(userProps.id);
+	const wrapper = shallow(<UnlockUser {...userProps} />);
+	wrapper.instance().fetchPatchData(apiUrl, false);
+	expect(userProps.onToggleLock).toHaveBeenCalledTimes(1);
 });
