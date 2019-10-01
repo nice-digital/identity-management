253f937c24de02273c5999686b522b25e3eab496
Cheguegallagher <martin.gallagher@nice.org.uk>
2019-10-01 11:46:35 +0100

    Restructured the call to api for deletion

--- functional-tests/src/support/action/deleteUser.js
+++ functional-tests/src/support/action/deleteUser.js
@@ -11,8 +11,10 @@ module.exports = (username, userApi) => {
     if (this.status == 200) {
       const deleteUserRequest = new XMLHttpRequest();
       const users = JSON.parse(this.responseText);
-      const myUser = users.find(user => user.email === process.env[username]);
-      deleteUserRequest.open('DELETE', process.env[userApi] + '/users' + '?userId=' + myUser.userId, false);
+      const myUser = users.find(user => user.emailAddress === process.env[username]);
+      console.log('myUser ', myUser.emailAddress);
+      console.log
+      deleteUserRequest.open('DELETE', process.env[userApi] + '/users/' + myUser.userId, false);
       deleteUserRequest.setRequestHeader('Authorization', 'Bearer ' + process.env.access_token);
       deleteUserRequest.setRequestHeader('Content-Type', 'application/json');
       deleteUserRequest.onload = function(e) {
