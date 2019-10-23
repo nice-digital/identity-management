1ab4b9c66387d920c33370fd40a88d78f49193fd
John Holland <john.holland@nice.org.uk>
2019-10-23 16:27:14 +0100

    IDAM-248 - Added idam into global nav

--- NICE.Identity.Management/ClientApp/src/components/App/App.tsx
+++ NICE.Identity.Management/ClientApp/src/components/App/App.tsx
@@ -8,11 +8,32 @@ import { User } from "./../../views/User/User";
 import { UserRoles } from "./../../views/UserRoles/UserRoles";
 import { DeleteUser } from "./../../views/DeleteUser/DeleteUser";
 
+type LinkType = {
+	key: String;
+	value: String;
+};
+
+type AuthType = {
+	links: Array<LinkType>;
+	displayName: String;
+	provider?: String;
+};
+
 export class App extends React.Component {
 	render() {
+		const links: Array<LinkType> = [
+			{ key: "Sign out", value: "/Account/Logout" },
+		];
+
+		const auth: any = {
+			links: [{ key: "Sign out", value: "/Account/Logout" }],
+			displayName: "Alice",
+			provider: "idam",
+		};
+
 		return (
 			<Router>
-				<Header search={false} />
+				<Header search={false} auth={auth} />
 
 				<div className="container">
 					<Route path="/" exact render={() => <Redirect to="/users" />} />
