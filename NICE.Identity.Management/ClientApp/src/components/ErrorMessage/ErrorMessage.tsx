84ce4906e7e2ca7c34105dba2d7da28785fdc64d
Jose Hilario Almeida <josealmeida@gmail.com>
2019-10-28 13:55:43 +0000

    IDAM-285 Display error details in error message

--- NICE.Identity.Management/ClientApp/src/components/ErrorMessage/ErrorMessage.tsx
+++ NICE.Identity.Management/ClientApp/src/components/ErrorMessage/ErrorMessage.tsx
@@ -17,14 +17,11 @@ export const ErrorMessage = (props: ErrorMessageProps) => (
 				<p>Sorry, there's been an error!</p>
 			</>
 		)}
-
-		{process.env.NODE_ENV === "development" && (
-			<dl>
-				<dt>Message:</dt>
-				<dd>{props.error.message}</dd>
-				<dt>Stack:</dt>
-				<dd>{props.error.stack}</dd>
-			</dl>
-		)}
+        <dl style={{visibility:"hidden"}}>
+            <dt>Message:</dt>
+            <dd>{props.error.message}</dd>
+            <dt>Stack:</dt>
+            <dd>{props.error.stack}</dd>
+        </dl>
 	</Alert>
 );
