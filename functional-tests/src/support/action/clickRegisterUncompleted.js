f7254587dfb84a1c290d661150f257db100ea644
Cheguegallagher <martin.gallagher@nice.org.uk>
2019-10-30 15:24:19 +0000

    Registration validation messages test

---
+++ functional-tests/src/support/action/clickRegisterUncompleted.js
@@ -0,0 +1,13 @@
+import setInputField from "@nice-digital/wdio-cucumber-steps/lib/support/action/setInputField";
+import waitForVisible from "@nice-digital/wdio-cucumber-steps/lib/support/action/waitForVisible";
+import click from "@nice-digital/wdio-cucumber-steps/lib/support/action/clickElement";
+import waitFor from "@nice-digital/wdio-cucumber-steps/lib/support/action/waitFor";
+import checkEqualsText from "@nice-digital/wdio-cucumber-steps/lib/support/check/checkEqualsText";
+import selectors from "../selectors";
+
+export const clickRegisterUncompleted = () => {
+  click('click', 'element', selectors.registrationPage.registerButton);
+  browser.pause(10000);
+}
+
+export default clickRegisterUncompleted;
--- functional-tests/src/support/check/validateMessages.js
+++ functional-tests/src/support/check/validateMessages.js
@@ -11,5 +11,16 @@ export const validateDeletionSuccessMessage = (message) => {
   pause(1000);
 };
 
+export const validateRegistrationValidationMessages = () => {
+  pause(1000);
+  checkContainsText("element", selectors.registrationPage.emailValidationMessage, "This field is required");
+  checkContainsText("element", selectors.registrationPage.confirmEmailValidationMessage, "Email address doesn't match");
+  checkContainsText("element", selectors.registrationPage.passwordValidationMessage, "This field is required");
+  checkContainsText("element", selectors.registrationPage.confirmPasswordValidationMessage, "Password doesn't match");
+  checkContainsText("element", selectors.registrationPage.firstNameValidationMessage, "This field is required");
+  checkContainsText("element", selectors.registrationPage.lastNameValidationMessage, "This field is required");
+}
+
+
 export default validateDeletionSuccessMessage;
 
