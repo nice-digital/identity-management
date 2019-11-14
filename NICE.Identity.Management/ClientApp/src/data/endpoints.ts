bfbd76ef038f9279aae90e2ab9f22b275ee8be04
John Holland <john.holland@nice.org.uk>
2019-11-14 11:49:12 +0000

    IDAM-57 Added new endpoint urls

--- NICE.Identity.Management/ClientApp/src/data/endpoints.ts
+++ NICE.Identity.Management/ClientApp/src/data/endpoints.ts
@@ -4,11 +4,17 @@ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
 
 export const Endpoints = {
 	usersList: `${API_BASE_URL}/users`, // get all users
-	user: (id: string | number) => {
-		return `${API_BASE_URL}/users/${id}`;
-	},
+	user: (userId: string | number) => `${API_BASE_URL}/users/${userId}`,
+
 	servicesList: `${API_BASE_URL}/services`,
-	service: (serviceId: string | number) => {
-		return `${API_BASE_URL}/services/${serviceId}`;
-	},
+	service: (serviceId: string | number) =>
+		`${API_BASE_URL}/services/${serviceId}`,
+
+	userRolesList: `${API_BASE_URL}/userRoles/`,
+	userRole: (userRoleId: string | number) =>
+		`${API_BASE_URL}/userRoles/${userRoleId}`,
+
+	userRolesByWebsiteList: `${API_BASE_URL}/userRolesByWebsite/`,
+	userRolesByWebsite: (websiteId: string | number) =>
+		`${API_BASE_URL}/userRolesByWebsite/${websiteId}`,
 };
