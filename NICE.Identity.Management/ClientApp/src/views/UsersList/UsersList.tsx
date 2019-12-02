6b7a46443cfc86bf9b0247a51279952f56e4753d
John Holland <john.holland@nice.org.uk>
2019-12-02 16:44:24 +0000

    IDAM-57 - Fixed tests

--- NICE.Identity.Management/ClientApp/src/views/UsersList/UsersList.tsx
+++ NICE.Identity.Management/ClientApp/src/views/UsersList/UsersList.tsx
@@ -99,7 +99,7 @@ export class UsersList extends Component<UsersListProps, UsersListState> {
 											<Card
 												{...usersListHeading}
 												metadata={usersListMetadata}
-												key={auth0UserId}
+												key={nameIdentifier}
 											/>
 										);
 									})}
