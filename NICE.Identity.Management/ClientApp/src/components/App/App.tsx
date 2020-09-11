22face7b078d3c8adba268d1933e2e74f33f7a65
Eleanor Mollett <eleanor.mollett@gmail.com>
2020-09-11 16:26:17 +0100

    IDAM-401 Added content start

--- NICE.Identity.Management/ClientApp/src/components/App/App.tsx
+++ NICE.Identity.Management/ClientApp/src/components/App/App.tsx
@@ -22,7 +22,7 @@ export class App extends React.Component {
 			<Router>
 				<Header search={false} auth={auth} />
 
-				<main className="container" role="main">
+				<main className="container" role="main"  id="content-start" aria-label="Start of content">
 					<Route path="/" exact render={() => <Redirect to="/users" />} />
 					<Route path="/users" exact component={UsersList} />
 					<Route path="/users/:id" exact component={User} />
