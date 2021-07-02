b2e69de0a8cebe4ec453a5195766a799c545acfe
John Holland <john.holland@nice.org.uk>
2021-07-02 09:46:37 +0100

    IDAM-445 Fix tests WIP

--- NICE.Identity.Management/ClientApp/src/setupTests.js
+++ NICE.Identity.Management/ClientApp/src/setupTests.js
@@ -1,7 +1,10 @@
 import { configure } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { enableFetchMocks } from "jest-fetch-mock";
 
 configure({ adapter: new Adapter() });
+
+enableFetchMocks();
 
 // https://twitter.com/kentcdodds/status/1163644392789250048?s=19
 beforeEach(() => {
@@ -10,6 +13,7 @@ beforeEach(() => {
 		throw new Error("This must be mocked!");
 	});
 });
+
 afterEach(() => {
 	window.fetch.mockRestore();
 });
