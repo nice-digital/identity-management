ca338c6b2402187096230d49839404575d5c0db1
Cheguegallagher <martin.gallagher@nice.org.uk>
2021-07-28 09:22:06 +0100

    Trying to fix npm error

--- functional-tests/src/support/action/selectService.js
+++ functional-tests/src/support/action/selectService.js
@@ -1,40 +1,44 @@
-import clickElement from "@nice-digital/wdio-cucumber-steps/lib/support/action/clickElement";
-import waitForVisible from "@nice-digital/wdio-cucumber-steps/lib/support/action/waitForVisible";
-import selectors from "../selectors";
+import clickElement from '@nice-digital/wdio-cucumber-steps/lib/support/action/clickElement';
+import waitForVisible from '@nice-digital/wdio-cucumber-steps/lib/support/action/waitForVisible';
+import selectors from '../selectors';
 
 export const clickIdamDockerService = () => {
   waitForVisible(selectors.serviceListPage.thirdService);
-  clickElement("click", "selector", selectors.serviceListPage.thirdService);
+  clickElement('click', 'selector', selectors.serviceListPage.thirdService);
 };
 
 export const clickTestEnvironment = () => {
   waitForVisible(selectors.environmentListPage.firstEnvironment);
-  clickElement("click", "selector", selectors.environmentListPage.firstEnvironment);
+  clickElement(
+    'click',
+    'selector',
+    selectors.environmentListPage.firstEnvironment
+  );
 };
 
 export const clickFirstRole = () => {
   waitForVisible(selectors.rolesListPage.firstRole);
-  clickElement("click", "selector", selectors.rolesListPage.firstRole);
+  clickElement('click', 'selector', selectors.rolesListPage.firstRole);
 };
 
 export const clickSecondRole = () => {
   waitForVisible(selectors.rolesListPage.secondRole);
-  clickElement("click", "selector", selectors.rolesListPage.secondRole);
+  clickElement('click', 'selector', selectors.rolesListPage.secondRole);
 };
 
 export const clickThirdRole = () => {
   waitForVisible(selectors.rolesListPage.thirdRole);
-  clickElement("click", "selector", selectors.rolesListPage.thirdRole);
+  clickElement('click', 'selector', selectors.rolesListPage.thirdRole);
 };
 
 export const clickFourthRole = () => {
   waitForVisible(selectors.rolesListPage.fourthRole);
-  clickElement("click", "selector", selectors.rolesListPage.fourthRole);
+  clickElement('click', 'selector', selectors.rolesListPage.fourthRole);
 };
 
 export const clickSave = () => {
   waitForVisible(selectors.rolesListPage.saveButton);
-  clickElement("click", "selector", selectors.rolesListPage.saveButton);
+  clickElement('click', 'selector', selectors.rolesListPage.saveButton);
 };
 
 export default clickIdamDockerService;
