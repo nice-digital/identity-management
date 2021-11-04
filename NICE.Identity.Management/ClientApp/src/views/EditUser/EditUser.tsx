ebbc7ef8ee839bd963eeb2b5150f103cf0714dee
John Holland <john.holland@nice.org.uk>
2021-11-04 12:27:32 +0000

    IDAM-268 Edit user WIP
    Form/validation now works with mock api

--- NICE.Identity.Management/ClientApp/src/views/EditUser/EditUser.tsx
+++ NICE.Identity.Management/ClientApp/src/views/EditUser/EditUser.tsx
@@ -1,9 +1,12 @@
-import React from "react";
-import { RouteComponentProps, Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { RouteComponentProps, Link, Redirect } from "react-router-dom";
 
 import { Grid, GridItem } from "@nice-digital/nds-grid";
 import { Breadcrumbs, Breadcrumb } from "@nice-digital/nds-breadcrumbs";
 import { Button } from "@nice-digital/nds-button";
+import { FormGroup } from "@nice-digital/nds-form-group";
+import { Input } from "@nice-digital/nds-input";
+import { Radio } from "@nice-digital/nds-radio";
 import { PageHeader } from "@nice-digital/nds-page-header";
 
 import { useFetch } from "../../helpers/useFetch";
@@ -12,18 +15,139 @@ import { UserType } from "../../models/types";
 
 import { ErrorMessage } from "../../components/ErrorMessage/ErrorMessage";
 
+import "@nice-digital/nds-radio/scss/radio.scss";
+
 type TParams = { id: string };
 
 type EditUserProps = Record<string, unknown> & RouteComponentProps<TParams>;
 
+type EditUserForm = Record<string, HTMLInputElement>;
+
 export const EditUser = (props: EditUserProps): React.ReactElement => {
+	const { id } = props.match.params;
+
+	const [isSaveButtonLoading, setIsSaveButtonLoading] = useState(false);
+	const [errorList, setErrorList] = useState<any>({});
+	const [redirect, setRedirect] = useState(false);
 	const {
 		data: user,
 		isLoading,
 		error,
-	} = useFetch<UserType>(Endpoints.user(props.match.params.id));
+		doFetch,
+	} = useFetch<UserType>(Endpoints.user(id));
 
-	const { id } = props.match.params;
+	useEffect(() => {
+		let isChanged = false;
+
+		doFetch();
+
+		// cleanup for unmounted console error
+		return () => {
+			isChanged = true;
+		};
+	}, []);
+
+	/*
+		const validate = (
+			form: HTMLFormElement,
+			formElements: Array<any>,
+		): boolean => {
+			const validationErrors = {} as Record<string, boolean>;
+
+			formElements.forEach((element) => {
+				validationErrors[element.name] = !element.validity.valid;
+			});
+
+			setErrorList(validationErrors);
+
+			return form.checkValidity();
+		};
+	*/
+
+	const formHasChanged = ({
+		emailAddress,
+		firstName,
+		lastName,
+		audienceInsight_optIn,
+	}: EditUserForm): boolean => {
+		const userDetails = [
+			user.emailAddress,
+			user.firstName,
+			user.lastName,
+			user.allowContactMe,
+		];
+		const formDetails = [
+			emailAddress.value,
+			firstName.value,
+			lastName.value,
+			audienceInsight_optIn.checked,
+		];
+
+		const valuesAreDifferent = !!userDetails.filter(
+			(x) => !formDetails.includes(x),
+		).length;
+
+		return valuesAreDifferent;
+	};
+
+	const handleSubmit = async (
+		e: React.FormEvent<HTMLFormElement>,
+	): Promise<void | boolean> => {
+		e.preventDefault();
+
+		setIsSaveButtonLoading(true);
+
+		const form = e.currentTarget;
+		const { emailAddress, firstName, lastName, audienceInsight_optIn } = form;
+
+		if (!form.checkValidity()) {
+			return false;
+		}
+
+		if (!formHasChanged(form)) {
+			setRedirect(true);
+
+			return false;
+		}
+
+		const fetchOptions = {
+			method: "PATCH",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				userId: id,
+				emailAddress: emailAddress.value,
+				firstName: firstName.value,
+				lastName: lastName.value,
+				allowContactMe: audienceInsight_optIn.checked,
+			}),
+		};
+
+		await doFetch(Endpoints.user(id), fetchOptions);
+
+		setRedirect(true);
+
+		setIsSaveButtonLoading(false);
+	};
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+		const formElement = e.currentTarget;
+		const validationErrors = { ...errorList };
+
+		if (validationErrors[formElement.name] && formElement.validity.valid) {
+			validationErrors[formElement.name] = false;
+		}
+
+		setErrorList(validationErrors);
+	};
+
+	const handleBlur = (e: React.FocusEvent<HTMLInputElement>): void => {
+		const formElement = e.currentTarget;
+		const validationErrors = { ...errorList };
+
+		validationErrors[formElement.name] = !formElement.validity.valid;
+
+		setErrorList(validationErrors);
+	};
 
 	const usernameBreadcrumb = isLoading
 		? "Loading user details"
@@ -49,36 +173,99 @@ export const EditUser = (props: EditUserProps): React.ReactElement => {
 
 			{!error ? (
 				<>
-					{isLoading ? (
+					{isLoading && !isSaveButtonLoading ? (
 						<>
 							<PageHeader preheading="Confirm" heading="Edit user" />
 							<p>Loading...</p>
 						</>
 					) : (
-						<PageHeader
-							preheading="Personal details for"
-							heading={`${user.firstName} ${user.lastName}`}
-							cta={
-								<>
-									<Button
-										data-qa-sel="edit-user-profile"
-										variant="cta"
-										// onClick={this.handleDeleteClick}
-										// disabled={isDeleteButtonLoading}
-									>
-										Save profile
-									</Button>
-									<Button
-										to={`/users/${id}`}
-										variant="secondary"
-										elementType={Link}
-										// disabled={isDeleteButtonLoading}
-									>
-										Cancel
-									</Button>
-								</>
-							}
-						/>
+						<>
+							{redirect && <Redirect to={`/users/${id}`} />}
+
+							<PageHeader
+								preheading="Personal details for"
+								heading={`${user.firstName} ${user.lastName}`}
+							/>
+							<Grid>
+								<GridItem cols={12} md={4}>
+									<form onSubmit={handleSubmit} noValidate>
+										<Input
+											label="Email address"
+											name="emailAddress"
+											type="email"
+											required
+											onBlur={handleBlur}
+											onChange={handleChange}
+											error={errorList.emailAddress}
+											errorMessage="Email address is in an invalid format"
+											defaultValue={user.emailAddress}
+										/>
+										<Input
+											label="First name"
+											name="firstName"
+											type="text"
+											required
+											minLength="2"
+											maxLength="100"
+											pattern="^((?![<>]).)*$"
+											onBlur={handleBlur}
+											onChange={handleChange}
+											error={errorList.firstName}
+											errorMessage="First name should contain letters and should not exceed 100 characters"
+											defaultValue={user.firstName}
+										/>
+										<Input
+											label="Last name"
+											name="lastName"
+											type="text"
+											required
+											minLength="2"
+											maxLength="100"
+											pattern="^((?![<>]).)*$"
+											onBlur={handleBlur}
+											onChange={handleChange}
+											error={errorList.lastName}
+											errorMessage="Last name should contain letters and should not exceed 100 characters"
+											defaultValue={user.lastName}
+										/>
+
+										<div className="form-group-container form-group-container--no-border">
+											<FormGroup
+												legend="Audience insight community membership"
+												name="audienceInsight"
+											>
+												<Radio
+													value="optIn"
+													label="Opt in"
+													defaultChecked={user.allowContactMe}
+												/>
+												<Radio
+													value="optOut"
+													label="Opt out"
+													defaultChecked={!user.allowContactMe}
+												/>
+											</FormGroup>
+											<Button
+												data-qa-sel="edit-user-profile"
+												variant="cta"
+												type="submit"
+												disabled={isLoading}
+											>
+												Save profile
+											</Button>
+											<Button
+												to={`/users/${id}`}
+												variant="secondary"
+												elementType={Link}
+												disabled={isLoading}
+											>
+												Cancel
+											</Button>
+										</div>
+									</form>
+								</GridItem>
+							</Grid>
+						</>
 					)}
 				</>
 			) : (
@@ -87,11 +274,3 @@ export const EditUser = (props: EditUserProps): React.ReactElement => {
 		</>
 	);
 };
-
-{
-	/* <Grid equalHeight>
-	<GridItem cols={12} md={4} className="mb--d">
-		{`${user.firstName} ${user.lastName}`}
-	</GridItem>
-</Grid> */
-}
