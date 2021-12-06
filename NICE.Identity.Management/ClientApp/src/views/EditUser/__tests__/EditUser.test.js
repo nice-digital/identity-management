2bc17f870a642738e56fa6444292eda47f75f9b6
John Holland <john.holland@nice.org.uk>
2021-12-06 22:34:17 +0000

    IDAM-268 Unit tests for edit user

--- NICE.Identity.Management/ClientApp/src/views/EditUser/__tests__/EditUser.test.js
+++ NICE.Identity.Management/ClientApp/src/views/EditUser/__tests__/EditUser.test.js
@@ -3,10 +3,13 @@ import { mount, shallow } from "enzyme";
 import { MemoryRouter } from "react-router";
 import { Redirect } from "react-router-dom";
 import toJson from "enzyme-to-json";
+import { Input } from "@nice-digital/nds-input";
 
 import { EditUser } from "../EditUser";
 import singleUser from "./singleUser.json";
 import { nextTick } from "../../../utils/nextTick";
+
+import { ErrorMessage } from "../../../components/ErrorMessage/ErrorMessage";
 
 describe("EditUser", () => {
 	const match = {
@@ -53,12 +56,9 @@ describe("EditUser", () => {
 		expect(wrapper.find("button").text()).toEqual("Loading...");
 	});
 
-	it("should display confirmation message once submit is complete", async () => {
+	it("should redirect if form is submitted without changes", async () => {
 		console.error = jest.fn();
-		const singleUserEdited = { ...singleUser };
-		singleUserEdited.firstName = "John";
 		fetch.mockResponseOnce(JSON.stringify(singleUser));
-		fetch.mockResponseOnce(JSON.stringify(singleUserEdited));
 		const wrapper = mount(
 			<MemoryRouter>
 				<EditUser match={match} />
@@ -71,11 +71,130 @@ describe("EditUser", () => {
 			currentTarget: { 
 				checkValidity: () => true
 			}
-			//target: { emailAddress: { value: "johnz.olland@outlook.com" }},
 		});
 		await nextTick();
 		wrapper.update();
 		expect(wrapper.find(Redirect).exists()).toBe(true);
 	});
 
+	it("should show error message when useFetch get call returns 401 error", async () => {	
+		console.error = jest.fn();
+		fetch.mockResponseOnce(JSON.stringify({}), { status: 401 });
+		const wrapper = mount(
+			<MemoryRouter>
+				<EditUser match={match} />
+			</MemoryRouter>,
+		);
+		await nextTick();
+		wrapper.update();
+		expect(toJson(wrapper, { noKey: true, mode: "deep" })).toMatchSnapshot();
+	});
+
+	it("should show error message when useFetch get call returns 500 error", async () => {
+		console.error = jest.fn();
+		fetch.mockRejectOnce(new Error("500 Internal Server Error"));
+		const wrapper = mount(
+			<MemoryRouter>
+				<EditUser match={match} />
+			</MemoryRouter>,
+		);
+		await nextTick();
+		wrapper.update();
+		expect(toJson(wrapper, { noKey: true, mode: "deep" })).toMatchSnapshot();
+	});
+
+	it("should show error message when useFetch patch fails", async () => {
+		console.error = jest.fn();
+		const error = new Error("Not allowed");
+		fetch.mockResponseOnce(JSON.stringify(singleUser));
+		fetch.mockRejectOnce(error);		
+		const wrapper = mount(
+			<MemoryRouter>
+				<EditUser match={match} />
+			</MemoryRouter>,
+		);
+		await nextTick();
+		wrapper.update();
+		wrapper.find("#emailAddress").prop("onChange")({
+			target: {
+				name: "emailAddress",
+				value: "john.holland@nice.org.uk",
+				validity: { valid: true }
+			}
+		});
+		await nextTick();
+		wrapper.update();
+		wrapper.find("form").prop("onSubmit")({
+			preventDefault: () => false, 
+			currentTarget: { 
+				checkValidity: () => true
+			},
+		});
+		await nextTick();
+		wrapper.update();
+		expect(wrapper.find(ErrorMessage).exists()).toBe(true);
+	});
+	
+	it("should show validation error when email is invalid format", async () => {
+		console.error = jest.fn();
+		fetch.mockResponseOnce(JSON.stringify(singleUser));
+		const wrapper = mount(
+			<MemoryRouter>
+				<EditUser match={match} />
+			</MemoryRouter>,
+		);
+		await nextTick();
+		wrapper.update();
+		wrapper.find("#emailAddress").prop("onChange")({
+			target: {
+				name: "emailAddress",
+				value: "john.holland@",
+				validity: { valid: false }
+			}
+		});
+		await nextTick();
+		wrapper.update();		
+		wrapper.find("#emailAddress").prop("onBlur")({
+			target: {
+				name: "emailAddress",
+				value: "john.holland@",
+				validity: { valid: false }
+			}
+		});
+		await nextTick();
+		wrapper.update();
+		expect(wrapper.find({ label: "Email address" }).prop("error")).toEqual(true);
+	});
+
+	it("should show validation error when email is in use already", async () => {
+		console.error = jest.fn();
+		fetch.mockResponseOnce(JSON.stringify(singleUser));
+		fetch.mockResponseOnce(JSON.stringify({ error: { message: "Multiple users found with same email address." }, status: 422 }));
+		const wrapper = mount(
+			<MemoryRouter>
+				<EditUser match={match} />
+			</MemoryRouter>,
+		);
+		await nextTick();
+		wrapper.update();
+		wrapper.find("#emailAddress").prop("onChange")({
+			target: {
+				name: "emailAddress",
+				value: "john.holland@hotmail.com",
+				validity: { valid: true }
+			}
+		});
+		await nextTick();
+		wrapper.update();		
+		wrapper.find("form").prop("onSubmit")({
+			preventDefault: () => false, 
+			currentTarget: { 
+				checkValidity: () => true
+			},
+		});
+		await nextTick();
+		wrapper.update();
+		expect(wrapper.find({ label: "Email address" }).prop("error")).toEqual(true);
+		expect(wrapper.find({ label: "Email address" }).prop("errorMessage")).toBe("Multiple users found with same email address.");
+	});
 });
