e6b99bca555f8757671904486dbb364f7451b445
John Holland <john.holland@nice.org.uk>
2022-01-13 17:18:13 +0000

    IDAM-433 AddOrganisation WIP

--- NICE.Identity.Management/ClientApp/src/views/AddOrganisation/AddOrganisation.tsx
+++ NICE.Identity.Management/ClientApp/src/views/AddOrganisation/AddOrganisation.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Alert } from "@nice-digital/nds-alert";
 import { Breadcrumbs, Breadcrumb } from "@nice-digital/nds-breadcrumbs";
 import { Button } from "@nice-digital/nds-button";
 import { Grid, GridItem } from "@nice-digital/nds-grid";
@@ -10,47 +11,42 @@ import { fetchData } from "../../helpers/fetchData";
 import { isDataError } from "../../helpers/isDataError";
 import { ErrorMessage } from "../../components/ErrorMessage/ErrorMessage";
 
-type AddOrganisationProps = {};
 type AddOrganisationState = {
-	path: string;
 	formName: string;
+	validationError: boolean;
 	hasSubmitted: boolean;
 	error?: Error;
-	isLoading: boolean;
 	isSaveButtonLoading: boolean;
 };
 export class AddOrganisation extends Component<
-	AddOrganisationProps,
+	Record<string, never>,
 	AddOrganisationState
 > {
-	constructor(props: AddOrganisationProps) {
+	constructor(props = {}) {
 		super(props);
 
 		this.state = {
-			path: "",
 			formName: "",
+			validationError: false,
 			hasSubmitted: false,
-			isLoading: false,
 			isSaveButtonLoading: false,
 		};
 
 		document.title = "NICE Accounts - Add organisation";
 	}
 
-	componentDidMount(): void {
-		this.setState({ isLoading: true });
-
-		// something
-
-		this.setState({ isLoading: false });
-	}
-
 	handleSubmit = async (
 		e: React.FormEvent<HTMLFormElement>,
 	): Promise<void | boolean> => {
 		e.preventDefault();
-		// do something
 		this.setState({ isSaveButtonLoading: true });
+
+		const form = e.currentTarget;
+
+		if (!form.checkValidity()) {
+			this.setState({ validationError: true, isSaveButtonLoading: false });
+			return false;
+		}
 
 		const fetchOptions = {
 			method: "POST",
@@ -60,9 +56,7 @@ export class AddOrganisation extends Component<
 			}),
 		};
 
-		//const data = await doFetch<UserType>(Endpoints.user(id), fetchOptions);
-
-		let organisation = await fetchData(
+		const organisation = await fetchData(
 			Endpoints.organisationsList,
 			fetchOptions,
 		);
@@ -71,27 +65,38 @@ export class AddOrganisation extends Component<
 			this.setState({ error: organisation });
 		}
 
-		this.setState({ isSaveButtonLoading: false, hasSubmitted: true });
+		this.setState({
+			isSaveButtonLoading: false,
+			hasSubmitted: true,
+			formName: "",
+		});
 	};
 
 	handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-		// do something else
-		this.setState({ formName: e.target.value });
+		const formElement = e.target;
+		const isNowValid = formElement.validity.valid;
+		let { validationError } = this.state;
+
+		if (validationError && isNowValid) {
+			validationError = false;
+		}
+
+		this.setState({ formName: e.target.value, validationError });
+	};
+
+	handleBlur = (e: React.ChangeEvent<HTMLInputElement>): void => {
+		const formElement = e.target;
+		const isNowValid = formElement.validity.valid;
+
+		this.setState({ validationError: !isNowValid });
 	};
 
 	render(): JSX.Element {
-		const {
-			path,
-			formName,
-			hasSubmitted,
-			error,
-			isLoading,
-			isSaveButtonLoading,
-		} = this.state;
+		const { formName, hasSubmitted, error, isSaveButtonLoading } = this.state;
 
-		if (hasSubmitted) {
-			return <Redirect to="/organisations" />;
-		}
+		// if (hasSubmitted) {
+		// 	return <Redirect to="/organisations" />;
+		// }
 
 		return (
 			<>
@@ -108,40 +113,60 @@ export class AddOrganisation extends Component<
 				<PageHeader heading="Add organisation" />
 
 				{!error ? (
-					<Grid>
-						<GridItem cols={12} md={6} lg={4}>
-							<form onSubmit={this.handleSubmit} noValidate>
-								<Input
-									data-qa-sel="name-input-add-organisation"
-									label="Organisation name"
-									name="orgName"
-									type="text"
-									required
-									autoComplete="off"
-									onChange={this.handleChange}
-									error={false}
-									value={formName}
-									disabled={isSaveButtonLoading}
-								/>
-								<Button
-									data-qa-sel="save-button-add-organisation"
-									variant="cta"
-									type="submit"
-									disabled={isSaveButtonLoading}
-								>
-									{isSaveButtonLoading ? "Loading..." : "Save organisation"}
-								</Button>
-								<Button
-									to={`/organisations`}
-									variant="secondary"
-									elementType={Link}
-									disabled={isSaveButtonLoading}
-								>
-									Cancel
-								</Button>
-							</form>
-						</GridItem>
-					</Grid>
+					<>
+						{hasSubmitted && (
+							<Alert
+								type="info"
+								role="status"
+								aria-live="polite"
+								data-qa-sel="successful-message-add-organisation"
+							>
+								<p>New organisation has been added successfully.</p>
+								<Link to={`/organisations`}>
+									Back to organisation admin page
+								</Link>
+							</Alert>
+						)}
+						<Grid>
+							<GridItem cols={12} md={6} lg={4}>
+								<form onSubmit={this.handleSubmit} noValidate>
+									<Input
+										data-qa-sel="name-input-add-organisation"
+										label="Organisation name"
+										name="orgName"
+										type="text"
+										required
+										autoComplete="off"
+										minLength="2"
+										maxLength="100"
+										pattern="^((?![<>]).)*$"
+										onChange={this.handleChange}
+										onBlur={this.handleBlur}
+										error={this.state.validationError}
+										errorMessage="Organisation name should be alphanumeric and should not exceed 100 characters"
+										value={formName}
+										disabled={isSaveButtonLoading}
+									/>
+									<Button
+										data-qa-sel="save-button-add-organisation"
+										variant="cta"
+										type="submit"
+										disabled={isSaveButtonLoading}
+									>
+										{isSaveButtonLoading ? "Loading..." : "Save organisation"}
+									</Button>
+									<Button
+										to={`/organisations`}
+										variant="secondary"
+										elementType={Link}
+										disabled={isSaveButtonLoading}
+									>
+										Cancel
+									</Button>
+								</form>
+							</GridItem>
+						</Grid>
+					</>
 				) : (
 					<ErrorMessage error={error}></ErrorMessage>
 				)}
