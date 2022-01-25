0c265a0f6df98569e56da526b995b3ce888dabf1
imranazad <imran.azad@nice.org.uk>
2022-01-25 12:11:32 +0000

    add tests

--- NICE.Identity.Management/ClientApp/src/views/Organisation/Organisation.tsx
+++ NICE.Identity.Management/ClientApp/src/views/Organisation/Organisation.tsx
@@ -6,19 +6,17 @@ import { OrganisationType, UserType } from "../../models/types";
 import { Breadcrumbs, Breadcrumb } from "@nice-digital/nds-breadcrumbs";
 import { PageHeader } from "@nice-digital/nds-page-header";
 import { Grid, GridItem } from "@nice-digital/nds-grid";
-import { Alert } from "@nice-digital/nds-alert";
+
+import { Button } from "@nice-digital/nds-button";
+import { ErrorMessage } from "../../components/ErrorMessage/ErrorMessage";
+import { UserStatus } from "../../components/UserStatus/UserStatus";
+import { ToFormattedDateString } from "../../helpers/dateHelpers";
 
 import { fetchData } from "../../helpers/fetchData";
 import { isDataError } from "../../helpers/isDataError";
 import { Endpoints } from "../../data/endpoints";
 
-import { Button } from "@nice-digital/nds-button";
-
-import { ErrorMessage } from "../../components/ErrorMessage/ErrorMessage";
 import styles from "./Organisation.module.scss";
-
-import { UserStatus } from "../../components/UserStatus/UserStatus";
-import { ToFormattedDateString } from "../../helpers/dateHelpers";
 
 type TParams = { id: string };
 
@@ -59,108 +57,120 @@ export class Organisation extends Component<OrganisationProps, OrganisationState
 				this.setState({ error: users });
 			}
 
+			if (isDataError(organisation)) {
+				this.setState({ error: organisation });
+			}
+
 			this.setState({ organisation, users, isLoading: false });
 		}
 
 	render(): JSX.Element {
 		const { organisation, users, error, isLoading } = this.state;
-
+		const lastBreadcrumb = `${organisation.name}`;
 		return (
-		<>
-			
-			<Breadcrumbs>
-				<Breadcrumb to="/overview" elementType={Link}>
-					Administration
-				</Breadcrumb>
-				<Breadcrumb>Organisation</Breadcrumb>
-				<Breadcrumb>{organisation.name}</Breadcrumb>
-			</Breadcrumbs>
-
-			{!error ? (
-				<>
-					<PageHeader heading={organisation.name} className="page-header mb--d" />
-							<>
-								<Button
-									data-qa-sel="edit-organisation-button"
-									variant="cta"
-									to={`/organisations/${this.props.match.params.id}/edit`}
-									elementType={Link}
-									disabled={isLoading}
-								>
-									{isLoading ? "Loading..." : "Edit organisation"}
-								</Button>
-								<Button
-									data-qa-sel="edit-organisations-users-button"
-									to={`/users-organisations/${this.props.match.params.id}/edit`}
-									elementType={Link}
-									disabled={isLoading}
-								>
-									Edit users
-								</Button>
-							</>
-					<Grid>
-						<GridItem cols={12} md={9} aria-busy={isLoading}>
+			<>
+				<Breadcrumbs>
+					<Breadcrumb data-qa-sel="breadcrumb-organisation-link" to="/overview" elementType={Link}>
+						Administration
+					</Breadcrumb>
+					<Breadcrumb>Organisation</Breadcrumb>
+					<Breadcrumb>{lastBreadcrumb}</Breadcrumb>
+				</Breadcrumbs>
+				
+				{!error ? (
+					<>
+						<PageHeader	heading={ 
+							isLoading ? "Organisation details" : `${organisation.name}`} 
+							className="page-header mb--d" 
+/* 							cta={ reinstate this after the edit org and edit user pages are created
+								<>
+									<Button
+										data-qa-sel="edit-organisation-button"
+										variant="cta"
+										to={`/organisations/${this.props.match.params.id}/edit`}
+										elementType={Link}
+										disabled={isLoading}
+									>
+										{isLoading ? "Loading..." : "Edit organisation"}
+									</Button>
+									<Button
+										data-qa-sel="edit-organisations-users-button"
+										to={`/users-organisations/${this.props.match.params.id}/edit`}
+										elementType={Link}
+										disabled={isLoading}
+									>
+										Edit users
+									</Button>
+								</>
+							} */
+						/>
+						<Grid>
+							<GridItem cols={12} md={9} aria-busy={isLoading}>
+							{isLoading ? (
+									<p className="OrganisationDateAddedLoadingMsg">Loading...</p>
+								) : organisation.dateAdded != null ? (
 									<div className={`${styles.summaryList} pv--c`}>
-										<span className={styles.summaryListLabel}>
-											Date added
-										</span>
-										<span
-											className={styles.summaryListDetail}
-											data-qa-sel="dateAdded-organisation"
-										>
-											{ToFormattedDateString(organisation.dateAdded)}
-										</span>
-									</div>
-						</GridItem>
+									<span className={styles.summaryListLabel}>
+										Date added
+									</span>
+									<span
+										className={styles.summaryListDetail}
+										data-qa-sel="dateAdded-organisation"
+									>
+										{ToFormattedDateString(organisation.dateAdded)}
+									</span>
+								</div>
+								) : <p>This organisation does not have a date added value.</p>
+							}
+							</GridItem>
+							<GridItem cols={12} md={9} aria-busy={isLoading}>
+							{isLoading ? (
+									<p className="OrganisationUsersListLoadingMsg">Loading...</p>
+								) : users.length ? (
+										<div className={`${styles.summaryList} pv--c`}>
+											<span className={styles.summaryListLabel}>
+												Users
+											</span>
+											<span className={styles.summaryListDetail}>
+												<ul className="list--unstyled" data-qa-sel="list-of-users">
+													{users.map((user) => {
+													return (
+														<li key={user.userId}>
+															<UserStatus user={user} />
+															<span className={styles.userName}>{user.firstName + " " + user.lastName}</span>
+														</li>
+													);
+												})}
+												</ul>
+											</span>
+										</div>
+								) : <p>There are currently no users assigned to this organisation.</p>
+							}
+							</GridItem>
+						</Grid>
+						<hr className="mv--b" />
 
-						<GridItem cols={12} md={9} aria-busy={isLoading}>
-						{isLoading ? (
-								<p>Loading...</p>
-							) : users.length ? (
-									<div className={`${styles.summaryList} pv--c`}>
-										<span className={styles.summaryListLabel}>
-											Users
-										</span>
-										<span className={styles.summaryListDetail}>
-											<ul className="list--unstyled" data-qa-sel="list-of-users">
-												{users.map((user) => {
-												return (
-													<li key={user.userId}>
-														<UserStatus user={user} />
-														<span className={styles.userName}>{user.firstName + " " + user.lastName}</span>
-													</li>
-												);
-											})}
-											</ul>
-										</span>
-									</div>
-							) : <p>Three are currently no users assigned to this oragnisation</p>
-						}
-						</GridItem>
-					</Grid>
-					<hr className="mv--b" />
-
-					<h2 className="h3">Permanently delete this organisation</h2>
-					<p>
-						This organisation will no longer be available, and all data in associated with the
-						organisation will be permanently deleted.
-					</p>
-					<Link
-						data-qa-sel="delete-organisation-link"
-						to={`/organisations/${organisation.id}/delete`}
-						className="pv--c mb--d"
-						style={{ display: "inline-block" }}
-					>
-						Delete organisation
-					</Link> 
-				</>
-			) : (
-				<>
-					<PageHeader heading="Error" />
-					<ErrorMessage error={error}></ErrorMessage>
-				</>
-			)}
-		</>
-	);
+						<h2 className="h3">Permanently delete this organisation</h2>
+						<p>
+							This organisation will no longer be available, and all data in associated with the
+							organisation will be permanently deleted.
+						</p>
+						<Link
+							data-qa-sel="delete-organisation-link"
+							to={`/organisations/${organisation.id}/delete`}
+							className="pv--c mb--d"
+							style={{ display: "inline-block" }}
+						>
+							Delete organisation
+						</Link> 
+					</>
+				) : (
+					<>
+						<PageHeader heading="Error" />
+						<ErrorMessage error={error}></ErrorMessage>
+					</>
+				)}
+			</>
+		);
 	}
 }
