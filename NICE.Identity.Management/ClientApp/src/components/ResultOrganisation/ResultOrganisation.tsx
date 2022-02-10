c3bb4832756be7ebee39f267a56ed4650e233729
John Holland <john.holland@nice.org.uk>
2022-02-10 11:16:14 +0000

    IDAM-433 Created separate component for an list org result

---
+++ NICE.Identity.Management/ClientApp/src/components/ResultOrganisation/ResultOrganisation.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Moment from "moment";
+import { OrganisationType } from "../../models/types";
+import { Card } from "@nice-digital/nds-card";
+
+type CardMetaData = {
+	label?: string;
+	value: string;
+};
+
+type ResultsOrganisationProps = {
+	item: OrganisationType;
+};
+
+export const ResultOrganisation = ({
+	item,
+}: ResultsOrganisationProps): React.ReactElement => {
+	const { id, name, dateAdded } = item;
+	const organisationsListHeading = {
+		headingText: `${name}`,
+		link: {
+			elementType: Link,
+			destination: `/organisations/${id}`,
+		},
+	};
+
+	const organisationsListMetadata: Array<CardMetaData> = [
+		{
+			label: "Date created",
+			value: Moment(dateAdded).format("DD-MM-YYYY"),
+		},
+	];
+
+	return (
+		<Card {...organisationsListHeading} metadata={organisationsListMetadata} />
+	);
+};
