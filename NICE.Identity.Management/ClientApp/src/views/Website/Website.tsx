c8a125cb8b48d26bb613df5a92d37c44c46fcab1
Sue Garner <Sue.Garner@nice.org.uk>
2022-02-23 12:13:06 +0000

    IDAM-441 Fixed linting errors

--- NICE.Identity.Management/ClientApp/src/views/Website/Website.tsx
+++ NICE.Identity.Management/ClientApp/src/views/Website/Website.tsx
@@ -4,13 +4,22 @@ import { RouteComponentProps, Link } from "react-router-dom";
 
 import { Breadcrumbs, Breadcrumb } from "@nice-digital/nds-breadcrumbs";
 import { PageHeader } from "@nice-digital/nds-page-header";
-import { queryStringToObject, appendQueryParameter, removeQueryParameter, stripMultipleQueries } from "../../utils/querystring";
+import {
+	queryStringToObject,
+	appendQueryParameter,
+	removeQueryParameter,
+	stripMultipleQueries,
+} from "../../utils/querystring";
 import { Table } from "@nice-digital/nds-table";
 import { Grid, GridItem } from "@nice-digital/nds-grid";
 
 import { fetchData } from "../../helpers/fetchData";
 import { isDataError } from "../../helpers/isDataError";
-import { RoleType, UserAndRolesType, WebsiteUsersAndRolesType } from "../../models/types";
+import {
+	RoleType,
+	UserAndRolesType,
+	WebsiteUsersAndRolesType,
+} from "../../models/types";
 import { Endpoints } from "../../data/endpoints";
 import { FilterBox } from "../../components/FilterBox/FilterBox";
 import { ErrorMessage } from "../../components/ErrorMessage/ErrorMessage";
@@ -37,7 +46,7 @@ type WebsiteState = {
 };
 
 export class Website extends Component<WebsiteProps, WebsiteState> {
-    constructor(props: WebsiteProps) {
+	constructor(props: WebsiteProps) {
 		super(props);
 
 		const querystring = this.props.location.search;
@@ -70,26 +79,25 @@ export class Website extends Component<WebsiteProps, WebsiteState> {
 			pageNumber: pageNumber,
 			itemsPerPage: itemsPerPage,
 			rolesForFilter: [],
-			roleFiltersChecked: []
+			roleFiltersChecked: [],
 		};
 	}
 
-    async componentDidMount(): Promise<void> {
+	async componentDidMount(): Promise<void> {
 		this.setState({ isLoading: true });
 
-		const websiteUsersAndRoles = await fetchData(Endpoints.usersAndRolesByWebsite(this.props.match.params.id));
+		const websiteUsersAndRoles = await fetchData(
+			Endpoints.usersAndRolesByWebsite(this.props.match.params.id),
+		);
 
 		if (isDataError(websiteUsersAndRoles)) {
 			this.setState({ error: websiteUsersAndRoles });
 		} else {
-			const allRoles: Array<string> = []
-			websiteUsersAndRoles.allRoles
-				.slice(0)
-				.map((role: RoleType) => {
-					allRoles.push(role.name)
-				});	
-				
-			
+			const allRoles: Array<string> = [];
+			websiteUsersAndRoles.allRoles.slice(0).map((role: RoleType) => {
+				allRoles.push(role.name);
+			});
+
 			const rolesForFilter = allRoles.reduce(function (
 				accumulatedRoles: string[],
 				currentRoles: string,
@@ -107,48 +115,46 @@ export class Website extends Component<WebsiteProps, WebsiteState> {
 			document.title = `NICE Accounts - ${websiteUsersAndRoles.website.host}`;
 		}
 
-		this.setState({ websiteUsersAndRoles, usersAndRoles: websiteUsersAndRoles.usersAndRoles, isLoading: false });
+		this.setState({
+			websiteUsersAndRoles,
+			usersAndRoles: websiteUsersAndRoles.usersAndRoles,
+			isLoading: false,
+		});
 	}
 
 	getListOfUsersByFilteredRole = (
 		roleFiltersChecked: Array<string>,
-		usersAndRoles: Array<UserAndRolesType>
+		usersAndRoles: Array<UserAndRolesType>,
 	): Array<UserAndRolesType> => {
-		const usersfilteredByRoles: Array<UserAndRolesType> = []
-		let currentUsersRoles: Array<string> = []
+		const usersfilteredByRoles: Array<UserAndRolesType> = [];
+		let currentUsersRoles: Array<string> = [];
 
-		usersAndRoles
-			.slice(0)
-			.map((user: UserAndRolesType) => {
-				currentUsersRoles = []
-				user.roles
-					.slice(0)
-					.map((role) => {
-						currentUsersRoles.push(role.name)
-					})
+		usersAndRoles.slice(0).map((user: UserAndRolesType) => {
+			currentUsersRoles = [];
+			user.roles.slice(0).map((role) => {
+				currentUsersRoles.push(role.name);
+			});
 
-					const matchesFilter = roleFiltersChecked.every(element => {
-						return currentUsersRoles.includes(element)
-					});
+			const matchesFilter = roleFiltersChecked.every((element) => {
+				return currentUsersRoles.includes(element);
+			});
 
-					if (matchesFilter) {
-						usersfilteredByRoles.push(user)
-					}
-			});	
+			if (matchesFilter) {
+				usersfilteredByRoles.push(user);
+			}
+		});
 
-		return usersfilteredByRoles
-	}
+		return usersfilteredByRoles;
+	};
 
 	filterUsersByRoles = (role: string): void => {
 		this.setState({ isLoading: true });
 
 		let roleFiltersChecked = this.state.roleFiltersChecked,
-		pageNumber = this.state.pageNumber;
+			pageNumber = this.state.pageNumber;
 
 		roleFiltersChecked = roleFiltersChecked.includes(role)
-			? roleFiltersChecked.filter(
-					(roleFilter) => roleFilter !== role,
-					)
+			? roleFiltersChecked.filter((roleFilter) => roleFilter !== role)
 			: roleFiltersChecked.concat(role);
 
 		const itemsPerPage = Number(this.state.itemsPerPage)
@@ -172,7 +178,7 @@ export class Website extends Component<WebsiteProps, WebsiteState> {
 		);
 
 		this.setState({ usersAndRoles, isLoading: false, roleFiltersChecked });
-	}
+	};
 
 	pastPageRange = (
 		itemsPerPage: string | number,
@@ -200,7 +206,7 @@ export class Website extends Component<WebsiteProps, WebsiteState> {
 	): string => {
 		const amountPerPage = finish - start;
 		const paginationExtract =
-		usersCount > amountPerPage ? `${start + 1} to ${finish} of ` : "";
+			usersCount > amountPerPage ? `${start + 1} to ${finish} of ` : "";
 
 		return `Showing ${paginationExtract}${usersCount} user${
 			usersCount === 1 ? "" : "s"
@@ -273,10 +279,19 @@ export class Website extends Component<WebsiteProps, WebsiteState> {
 		});
 	};
 
-    render(): JSX.Element {
-		const { websiteUsersAndRoles, usersAndRoles, error, isLoading, pageNumber, itemsPerPage, } = this.state;
+	render(): JSX.Element {
+		const {
+			websiteUsersAndRoles,
+			usersAndRoles,
+			error,
+			isLoading,
+			pageNumber,
+			itemsPerPage,
+		} = this.state;
 
-		const lastBreadcrumb = isLoading ? "Loading service details" : websiteUsersAndRoles.website?.service?.name;
+		const lastBreadcrumb = isLoading
+			? "Loading service details"
+			: websiteUsersAndRoles.website?.service?.name;
 
 		const paginationPositions = this.getPaginateStartAndFinishPosition(
 			usersAndRoles?.length,
@@ -291,23 +306,27 @@ export class Website extends Component<WebsiteProps, WebsiteState> {
 		);
 
 		const usersPaginated = usersAndRoles?.length
-		? usersAndRoles.slice(paginationPositions.start, paginationPositions.finish)
-		: usersAndRoles;
+			? usersAndRoles.slice(
+					paginationPositions.start,
+					paginationPositions.finish,
+			)
+			: usersAndRoles;
 
-        return(
-            <>
+		return (
+			<>
 				<Breadcrumbs>
 					<Breadcrumb
-							data-qa-sel="breadcrumb-administration-link"
-							to="/"
-							elementType={Link}
-						>
+						data-qa-sel="breadcrumb-administration-link"
+						to="/"
+						elementType={Link}
+					>
 						Administration
 					</Breadcrumb>
-					<Breadcrumb data-qa-sel="breadcrumb-service-link"
-						to="/services" 
+					<Breadcrumb
+						data-qa-sel="breadcrumb-service-link"
+						to="/services"
 						elementType={Link}
-						>
+					>
 						Services
 					</Breadcrumb>
 					<Breadcrumb>{lastBreadcrumb}</Breadcrumb>
@@ -315,7 +334,11 @@ export class Website extends Component<WebsiteProps, WebsiteState> {
 				{!error ? (
 					<>
 						<PageHeader
-							heading={isLoading ? "Service name" : `${websiteUsersAndRoles.website.service.name} (${websiteUsersAndRoles.website.environment.name})`}
+							heading={
+								isLoading
+									? "Service name"
+									: `${websiteUsersAndRoles.website.service.name} (${websiteUsersAndRoles.website.environment.name})`
+							}
 						/>
 
 						{isLoading ? (
@@ -331,9 +354,17 @@ export class Website extends Component<WebsiteProps, WebsiteState> {
 										hideFilterPanelHeading={true}
 									/>
 								</GridItem>
-								<GridItem cols={12} md={9} aria-busy={true} >
-									<h2 className={styles.websiteSummary} data-qa-sel="users-returned">{paginationText}</h2>
-									<Table style={{ display: "table" }}  data-qa-sel='list-of-users'>
+								<GridItem cols={12} md={9} aria-busy={true}>
+									<h2
+										className={styles.websiteSummary}
+										data-qa-sel="users-returned"
+									>
+										{paginationText}
+									</h2>
+									<Table
+										style={{ display: "table" }}
+										data-qa-sel="list-of-users"
+									>
 										<thead>
 											<tr>
 												<th>User name</th>
@@ -343,36 +374,36 @@ export class Website extends Component<WebsiteProps, WebsiteState> {
 											</tr>
 										</thead>
 										<tbody>
-										{usersAndRoles.length ?  (
-											usersPaginated
-												.slice(0)
-												.map((user) => (
-													<tr key={user.user.nameIdentifier} className="userRecord">
+											{usersAndRoles.length ? (
+												usersPaginated.slice(0).map((user) => (
+													<tr
+														key={user.user.nameIdentifier}
+														className="userRecord"
+													>
 														<td>
 															{user.user.firstName} {user.user.lastName}
 														</td>
+														<td>{user.user.emailAddress}</td>
 														<td>
-															{user.user.emailAddress}
+															{user.roles.slice(0).map((roles, index) => {
+																if (
+																	this.state.roleFiltersChecked.includes(
+																		roles.name,
+																	) ||
+																	this.state.roleFiltersChecked.length === 0
+																) {
+																	return <div key={index}>{roles.name}</div>;
+																}
+															})}
 														</td>
-														<td>
-															{user.roles
-																.slice(0)
-																.map((roles, index) => {
-																	if (this.state.roleFiltersChecked.includes(roles.name) || this.state.roleFiltersChecked.length === 0) {
-																		return <div key={index}>{roles.name}</div>;
-																	}
-																})}
-														</td>
-														<td>
-															{user.user.isStaffMember ? "Yes" : "No"}
-														</td>
+														<td>{user.user.isStaffMember ? "Yes" : "No"}</td>
 													</tr>
-											))
-										) : (
-											<tr>
-												<td colSpan={4}>0 results found</td>
-											</tr>
-										)}
+												))
+											) : (
+												<tr>
+													<td colSpan={4}>0 results found</td>
+												</tr>
+											)}
 										</tbody>
 									</Table>
 									<Pagination
@@ -389,7 +420,7 @@ export class Website extends Component<WebsiteProps, WebsiteState> {
 				) : (
 					<ErrorMessage error={error}></ErrorMessage>
 				)}
-            </>
-        );
-    }
+			</>
+		);
+	}
 }
