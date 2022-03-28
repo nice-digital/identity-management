43f69eaf4bfbe478790c1403470a0a48571d144e
imranazad <imran.azad@nice.org.uk>
2022-03-28 17:53:39 +0100

    IDAM-454 Remove Forceful HTTPS and instead rely on HSTS

--- NICE.Identity.Management/Startup.cs
+++ NICE.Identity.Management/Startup.cs
@@ -186,7 +186,7 @@ namespace NICE.Identity.Management
 				}
 			});
 
-			app.UseHttpsRedirection();
+			//app.UseHttpsRedirection();
 			//app.UseCookiePolicy();
 
 			app.UseRouting();
@@ -226,7 +226,7 @@ namespace NICE.Identity.Management
 				}
 			});
 
-			app.Use((context, next) =>
+			/*app.Use((context, next) =>
 			{
 				if (context.Request.Headers["X-Forwarded-Proto"] == "https" ||
 					context.Request.Headers["Front-End-Https"] == "on" ||
@@ -235,7 +235,7 @@ namespace NICE.Identity.Management
 					context.Request.Scheme = "https";
 				}
 				return next();
-			});
+			});*/
 
 			//app.UseRouting();
 
