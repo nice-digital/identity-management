a2a290975bee9930440fde045f3c4fa1669402d1
imranazad <imran.azad@nice.org.uk>
2022-04-07 15:07:56 +0100

    IDAM-453 Verrbose Server Headers

--- NICE.Identity.Management/Controllers/PermissionDeniedController.cs
+++ NICE.Identity.Management/Controllers/PermissionDeniedController.cs
@@ -8,7 +8,6 @@ namespace NICE.Identity.Management.Controllers
 {
 	public class PermissionDeniedController : Controller
 	{
-		[Produces("text/html")]
 		public IActionResult Index()
 		{
 			return View("PermissionDenied");
