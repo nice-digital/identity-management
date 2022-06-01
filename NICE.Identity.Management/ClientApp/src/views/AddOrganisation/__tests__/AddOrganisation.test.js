555fc0c119cecb0a03008d2ff23bc4edb0974527
John Holland <john.holland@nice.org.uk>
2022-06-01 15:02:12 +0100

    IDAM-511 Fixed last test for AddOrganisation

--- NICE.Identity.Management/ClientApp/src/views/AddOrganisation/__tests__/AddOrganisation.test.js
+++ NICE.Identity.Management/ClientApp/src/views/AddOrganisation/__tests__/AddOrganisation.test.js
@@ -1,161 +1,5 @@
-// import React from "react";
-// import { mount, shallow } from "enzyme";
-// import { MemoryRouter } from "react-router";
-// import toJson from "enzyme-to-json";
-// import { nextTick } from "../../../utils/nextTick";
-// import { Alert } from "@nice-digital/nds-alert";
-// import { AddOrganisation } from "../AddOrganisation";
-// import { ErrorMessage } from "../../../components/ErrorMessage/ErrorMessage";
-// import organisations from "./organisations.json";
-
-// describe("AddOrganisation", () => {
-// 	const consoleErrorReset = console.error;
-
-// 	beforeEach(() => {
-// 		fetch.resetMocks();
-// 		console.error = consoleErrorReset;
-// 	});
-
-//     it("should match the snapshot on load", () => {
-// 		const wrapper = shallow(<AddOrganisation />);
-// 		expect(toJson(wrapper, { noKey: true, mode: "deep" })).toMatchSnapshot();
-// 	});
-
-// 	it("should disable form submit button when clicked", async () => {
-// 		console.error = jest.fn();
-// 		fetch.mockResponseOnce(JSON.stringify({}));
-// 		const wrapper = mount(
-// 			<MemoryRouter>
-// 				<AddOrganisation />
-// 			</MemoryRouter>,
-// 		);
-// 		wrapper.find("#orgName").prop("onChange")({
-// 			target: {
-// 				name: "orgName",
-// 				value: "Org Ninety Nine",
-// 				validity: { valid: true }
-// 			}
-// 		});
-// 		wrapper.find("form").simulate("submit");
-// 		expect(wrapper.find("button").props().disabled).toEqual(true);
-// 		expect(wrapper.find("button").text()).toEqual("Loading...");
-// 	});
-
-// 	it("should display confirmation message once fetchData post is successfully complete", async () => {
-// 		console.error = jest.fn();
-// 		fetch.mockResponseOnce(JSON.stringify([]));
-// 		fetch.mockResponseOnce(JSON.stringify({}));
-// 		const wrapper = mount(
-// 			<MemoryRouter>
-// 				<AddOrganisation />
-// 			</MemoryRouter>,
-// 		);
-// 		wrapper.find("#orgName").prop("onChange")({
-// 			target: {
-// 				name: "orgName",
-// 				value: "Org Ninety Nine",
-// 				validity: { valid: true }
-// 			}
-// 		});
-// 		wrapper.find("form").prop("onSubmit")({
-// 			preventDefault: () => false, 
-// 			currentTarget: { 
-// 				checkValidity: () => true
-// 			},
-// 		});
-// 		await nextTick();
-// 		wrapper.update();
-// 		expect(wrapper.find("p").text()).toEqual("New organisation has been added successfully.");
-// 		expect(wrapper.find(Alert).exists()).toBe(true);
-// 	});
-
-//     it("should show error message when fetchData post fails", async () => {
-// 		console.error = jest.fn();
-// 		const error = new Error("Not allowed");
-// 		fetch.mockResponseOnce(JSON.stringify([]));
-// 		fetch.mockRejectOnce(error);
-// 		const wrapper = mount(
-// 			<MemoryRouter>
-// 				<AddOrganisation />
-// 			</MemoryRouter>,
-// 		);
-// 		wrapper.find("#orgName").prop("onChange")({
-// 			target: {
-// 				name: "orgName",
-// 				value: "Org Ninety Nine",
-// 				validity: { valid: true }
-// 			}
-// 		});
-// 		wrapper.find("form").prop("onSubmit")({
-// 			preventDefault: () => false, 
-// 			currentTarget: { 
-// 				checkValidity: () => true
-// 			},
-// 		});
-// 		await nextTick();
-// 		wrapper.update();
-// 		expect(wrapper.find(ErrorMessage).exists()).toBe(true);
-// 	});
-
-// 	it("should show validation error when name is invalid format", async () => {
-// 		console.error = jest.fn();
-// 		fetch.mockResponseOnce(JSON.stringify([]));
-// 		const wrapper = mount(
-// 			<MemoryRouter>
-// 				<AddOrganisation />
-// 			</MemoryRouter>,
-// 		);
-		
-// 		wrapper.find("#orgName").prop("onChange")({
-// 			target: {
-// 				name: "orgName",
-// 				value: "a",
-// 				validity: { valid: false }
-// 			}
-// 		});
-// 		wrapper.find("#orgName").prop("onBlur")({
-// 			target: {
-// 				name: "orgName",
-// 				value: "a",
-// 				validity: { valid: false }
-// 			}
-// 		});
-// 		await nextTick();
-// 		wrapper.update();
-// 		expect(wrapper.find({ label: "Organisation name" }).prop("error")).toEqual(true);
-// 	});
-
-// 	it("should show validation error when org name is in use already", async () => {
-// 		console.error = jest.fn();
-// 		fetch.mockResponseOnce(JSON.stringify(organisations));
-// 		const wrapper = mount(
-// 			<MemoryRouter>
-// 				<AddOrganisation />
-// 			</MemoryRouter>,
-// 		);
-// 		wrapper.find("#orgName").prop("onChange")({
-// 			target: {
-// 				name: "orgName",
-// 				value: "Org 1",
-// 				validity: { valid: true }
-// 			}
-// 		});
-// 		wrapper.find("#orgName").prop("onBlur")({
-// 			target: {
-// 				name: "orgName",
-// 				value: "Org 1",
-// 				validity: { valid: true }
-// 			}
-// 		});
-// 		await nextTick();
-// 		wrapper.update();
-// 		expect(wrapper.find({ label: "Organisation name" }).prop("error")).toEqual(true);
-// 		expect(wrapper.find({ label: "Organisation name" }).prop("errorMessage")).toBe("Cannot add Org 1, that organisation already exists!");
-// 	});
-
-// });
-
 import React from "react";
+import { act } from 'react-dom/test-utils';
 import { render, waitFor, fireEvent, screen } from "@testing-library/react";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
@@ -164,8 +8,6 @@ import userEvent from "@testing-library/user-event";
 import { AddOrganisation } from "../AddOrganisation";
 import { Endpoints } from "../../../data/endpoints";
 import organisations from "./organisations.json";
-import { act } from 'react-dom/test-utils';
-//import singleUser from "./singleUser.json";
 
 const consoleErrorReset = console.error;
 
@@ -242,7 +84,7 @@ test("should show validation error when name is invalid format", async () => {
   expect(orgNameInput).toBeInvalid();
 });
 
-test.only("should show validation error when org name is in use already", async () => {
+test("should show validation error when org name is in use already", async () => {
   jest.useFakeTimers();
   server.use(
     rest.get(Endpoints.organisationsList, (req, res, ctx) => {
@@ -254,21 +96,13 @@ test.only("should show validation error when org name is in use already", async 
   const orgNameInput = screen.getByLabelText("Organisation name");
   orgNameInput.focus();
   await act(async () => {
-    //user.type(orgNameInput, 'Org 1');
+    //user.paste("Org 1");
     fireEvent.change(orgNameInput, {target: {value: "Org 1"}});
-    jest.runOnlyPendingTimers();
+    jest.advanceTimersByTime(1000);
   });   
   user.tab();
-  // await waitFor(()=> {
-  //   user.type(orgNameInput, "Org 1");
-  // });
   const orgInUseMessage = await screen.findByText("Cannot add Org 1, that organisation already exists!");
-  screen.debug();
   expect(orgInUseMessage).toBeInTheDocument();
-  //await waitFor(() => screen.getByText("Cannot add Org 1, that organisation already exists!"), {timeout: 2000});
-  //screen.debug();
-  //expect(screen.getByText("Cannot add Org 1, that organisation already exists!")).toBeInTheDocument();
   jest.useRealTimers();
-  // something
 });
 
